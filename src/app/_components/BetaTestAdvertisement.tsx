69e3757beea0cc5eb5c59fd58c8d184b7e1b8935
Ray <rayc12079@gmail.com>
2024-06-25 13:18:17 -0400

    Added transition to pricing plan

--- src/app/_components/BetaTestAdvertisement.tsx
+++ src/app/_components/BetaTestAdvertisement.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 export default function BetaTestAdvertisement() {
   return (
     <Container className="bg-gradient-to-r from-blue-primary to-[#13B4C9] relative overflow-hidden !py-0 my-10">
-      <div className="relative max-w-[1440px] m-auto min-h-[350px]">
-        <div className="max-w-[75%] absolute top-1/2 -translate-y-1/2 space-y-2">
+      <div className="relative max-w-[1440px] m-auto min-h-[400px]">
+        <div className="md:max-w-[60%] absolute top-1/2 -translate-y-1/2 space-y-2 z-20">
           <h1 className="font-bold font-sora text-3xl">
             Apply to the Beta Test: Experience the Future Today!
           </h1>
@@ -25,7 +25,7 @@ export default function BetaTestAdvertisement() {
           width={0}
           height={0}
           sizes="100vw"
-          className="h-full w-auto absolute right-0 bottom-0 lg:opacity-100 opacity-25"
+          className="h-full w-auto absolute right-0 bottom-0 md:opacity-100 opacity-25 transition-opacity duration-300"
           alt="ai"
         />
       </div>
--- src/app/_components/Pricing.tsx
+++ src/app/_components/Pricing.tsx
@@ -1,18 +1,41 @@
+"use client";
 import Container from "@/app/_components/Container";
 import { pricing } from "../constants";
 import { FaCircleCheck } from "react-icons/fa6";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Pricing() {
+  useGSAP(() => {
+    gsap.to(".pricing-heading", {
+      opacity: 1,
+      y: 0,
+      scrollTrigger: {
+        trigger: ".pricing-heading",
+      },
+    });
+    gsap.to(".pricing-plan", {
+      opacity: 1,
+      x: 0,
+      stagger: { each: 0.25 },
+      scrollTrigger: {
+        trigger: ".pricing-plan",
+      },
+    });
+  }, []);
   return (
     <Container className="max-w-[1440px] m-auto">
-      <h2 className="font-sora text-3xl font-semibold text-center">
+      <h2 className="font-sora text-3xl font-semibold text-center pricing-heading opacity-0 translate-y-[15px]">
         Our Pricing Plans
       </h2>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 mt-10 gap-6 relative">
         {pricing.map((plan, i) => (
           <article
             key={i}
-            className={`bg-gray-darker border border-[#555555] p-6 rounded-lg relative ${plan?.additionalClassNames} z-20`}
+            className={`bg-gray-darker border border-[#555555] p-6 rounded-lg relative ${plan?.additionalClassNames} z-20 pricing-plan opacity-0 -translate-x-10`}
           >
             <h4 className="text-blue-primary text-center text-lg">
               [{plan.name}]
